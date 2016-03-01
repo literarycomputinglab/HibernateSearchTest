01bc5d2e18c0ff0caf9d9b172cc95b441a3316a9
angelo <angelo@dylan2>
2016-03-01 19:27:25 +0100

    Getting started

--- src/main/java/it/cnr/ilc/lc/hibernatesearchtest/Contact.java
+++ src/main/java/it/cnr/ilc/lc/hibernatesearchtest/Contact.java
@@ -10,67 +10,70 @@ package it.cnr.ilc.lc.hibernatesearchtest;
  * @author simone
  */
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
- 
+
 import org.hibernate.search.annotations.Analyze;
 import org.hibernate.search.annotations.Field;
 import org.hibernate.search.annotations.Index;
 import org.hibernate.search.annotations.Indexed;
 import org.hibernate.search.annotations.Store;
- 
+
 /**
  * The persistent class for the contact database table.
- * 
+ *
  */
 @Entity
 @Indexed
 @Table(name = "contact")
 public class Contact {
+
     private Integer id;
     private String name;
     private String email;
- 
+
     public Contact() {
- 
+
     }
- 
+
     public Contact(Integer id, String name, String email) {
         this.id = id;
         this.name = name;
         this.email = email;
     }
- 
+
     @Id
+    @GeneratedValue
     public Integer getId() {
         return this.id;
     }
- 
+
     public void setId(Integer id) {
         this.id = id;
     }
- 
+
     @Field(index = Index.YES, analyze = Analyze.YES, store = Store.NO)
     public String getName() {
         return this.name;
     }
- 
+
     public void setName(String name) {
         this.name = name;
     }
- 
+
     public String getEmail() {
         return email;
     }
- 
+
     public void setEmail(String email) {
         this.email = email;
     }
-     
+
     @Override
     public String toString() {
         StringBuilder stringBuilder = new StringBuilder("Id: ").append(this.getId()).append(" | Name:").append(this.getName()).append(" | Email:").append(this.getEmail());
-         
+
         return stringBuilder.toString();
     }
 }
